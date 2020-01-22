efdb6e98f6a906b216ec4a15c1dd5f1baa06523e
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2020-01-22 18:08:32 -0500

    sol 01

---
+++ ejercicios/01/ForeroJaime_Ejercicio01.py
@@ -0,0 +1,90 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+# defino una funcion para calcular un polinomio
+def poly(x, beta):
+    """
+    Entradas:
+    x : array de entrada.
+    beta: coeficientes del polinomio: beta[i] = beta_i. 
+
+    Salida:
+    y : array de salida
+    """
+    y = np.zeros(len(x))
+    for i in range(len(beta)):
+        y +=  beta[i] * x**i
+    return y
+
+def solve_poly(x, y, m):
+    """
+    Entradas:
+    x : array unidimensional de valores de x.
+    y : array unidimensiondal de valores de y.
+    m : orden del polinomio que queremos ajustar.
+
+    Salida:
+    beta : array con los coeficientes del polinotmi.
+
+
+    Este codigo resuelve el siguiente sistema de ecuaciones
+    S * beta = Y, beta = S^{-1} * Y, 
+    donde S es una matriz que tiene como vectotes a columnas de potencias de X.
+
+    """
+    # matriz que guarda los vectores columna de potencias de x
+    S = np.zeros([m+1, len(x)])
+    for i in range(m+1):
+        S[:,i] = x**i
+    
+    # Calculo la inversa de S.
+    S_inv = np.linalg.inv(S)
+    
+    # calculo beta
+    beta = np.dot(S_inv, y)
+    
+    return beta
+
+
+
+# cargo los datos para manipular
+data = np.loadtxt("numeros_20.txt")
+
+# diccionario para guardar todos los coeficientes
+betas = {}
+
+for m in [1,2,3,4]:
+    X = data[: m+1, 0]
+    Y = data[: m+1, 1]
+
+    betas[m] = solve_poly(X, Y, m)
+
+# preparo finalmene la grafica pedida
+
+plt.figure()
+
+for i in range(4):
+    # vuelvo a sacar los puntos para graficarlos
+    m = i+1
+    X = data[: m+1, 0]
+    Y = data[: m+1, 1]
+
+    # array de valores de x que va del minimo al maximo valor considerados
+    # para hacer los ajustes
+    x_model = np.linspace(np.min(data[:5,0]), np.max(data[:5,0]), 100)
+
+    # valores correspondientes al modelo de orden m
+    y_model = poly(x_model, betas[m])
+
+    plt.subplot(2,2,i+1)
+    #grafica del modelo
+    plt.plot(x_model, y_model)
+    #grafica de los puntos
+    plt.scatter(X, Y)
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("M={}".format(m))
+
+plt.subplots_adjust(hspace=0.5)
+plt.savefig("polinomios.png", bbox_inches='tight')
+
