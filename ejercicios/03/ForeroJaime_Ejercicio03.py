6629bdcc5957e264c67e0fb439dad870e0e9ba6b
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2020-01-29 18:13:59 -0500

    solucion

--- ejercicios/03/ForeroJaime_Ejercicio03.py
+++ ejercicios/03/ForeroJaime_Ejercicio03.py
@@ -18,8 +18,8 @@ data = np.loadtxt("notas_andes.dat", skiprows=1)
 Y = data[:,4]
 X = data[:,:4]
 
-# bootstrapping con 100 iteraciones
-n_intentos = 1000
+# bootstrapping con 10000 iteraciones
+n_intentos = 10000
 betas = np.ones([n_intentos, 4])
 for i in range(n_intentos):
     betas[i,:] = compute_betas(X, Y)
@@ -32,6 +32,7 @@ for i in range(4):
     plt.hist(betas[:,i], bins=20, density=True)
     plt.title(r"$\beta_{}= {:.2f}\pm {:.2f}$".format(i+1, np.mean(betas[:,i]), np.std(betas[:,i])))
     plt.xlabel(r"$\beta_{}$".format(i+1))
+    plt.xlim([-0.2, 0.4])
 
 plt.subplots_adjust(hspace=0.5)
 plt.savefig("bootstrapping", bbox_inches='tight')
