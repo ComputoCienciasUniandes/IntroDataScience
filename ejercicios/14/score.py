b884b63cc16e14760150966d91e46ea51eed7e85
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2020-03-06 09:16:52 -0500

    score

--- ejercicios/14/score.py
+++ ejercicios/14/score.py
@@ -8,5 +8,6 @@ predict = pd.read_csv("test/predict_test.csv")
 truth  = truth.sort_values('Name')
 predict = predict.sort_values('Name')
 
-f1 = sklearn.metrics.f1_score(truth['Target'], predict['Target'])
-print(f1)
+f1_0 = sklearn.metrics.f1_score(truth['Target'], predict['Target'], pos_label=0)
+f1_1 = sklearn.metrics.f1_score(truth['Target'], predict['Target'], pos_label=1)
+print(f1_0, f1_1)
