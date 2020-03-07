5b32212e170d6b438c46517c7c2e92a9af6a1e50
Jaime Forero-Romero <j.e.forero.romero@gmail.com>
2020-03-07 15:27:46 -0500

    sol

--- ejercicios/14/predict.py
+++ ejercicios/14/predict.py
@@ -1,16 +1,51 @@
 import glob
 import numpy as np
+import matplotlib.pyplot as plt
+import sklearn.discriminant_analysis
 
-files = glob.glob("train/*.jpg")
+def load_train():
+    n_files = 100
+    input_data = []
+    target = np.ones(n_files)
+    for i in range(n_files):
+        filename = "train/{}.jpg".format(i+1)
+        target[i] = i%2 
+        input_data.append(plt.imread(filename).flatten())
+    input_data = np.array(input_data)
+    print(np.shape(input_data), np.shape(target))
+    return input_data, target
 
-files_test = glob.glob("test/*.jpg")
-n_test = len(files_test)
-predict_test = np.int_(np.random.random(n_test)/0.5)
+def train():
+    x, y  = load_train()
+    clf = sklearn.discriminant_analysis.LinearDiscriminantAnalysis()
+    clf.fit(x,y)
+    return clf
 
-out = open("test/predict_test.csv", "w")
-out.write("Name,Target\n")
-for f, p in zip(files_test, predict_test):
-    print(f.split("/")[-1], p)
-    out.write("{},{}\n".format(f.split("/")[-1],p))
+def predict():
+    clf = train()
+    
+    files_test = glob.glob("test/*.jpg")
+    n_test = len(files_test)
+    test_data = []
+    for file_test in files_test:
+        test_data.append(plt.imread(file_test).flatten())
+    test_data = np.array(test_data)
 
-out.close()
+    predict_test = clf.predict(test_data)
+    out = open("test/predict_test.csv", "w")
+    out.write("Name,Target\n")
+    for f, p in zip(files_test, predict_test):
+        out.write("{},{}\n".format(f.split("/")[-1],p))
+    out.close()
+
+    # visualiza el vector discriminante
+    plt.figure()
+    discr = clf.coef_[0].reshape(100,100,3)
+    discr = discr-discr.min()
+    discr = np.int_(255*(discr/(discr.max() - discr.min())))
+    print(discr.min(), discr.max())
+    plt.imshow(discr)
+    plt.savefig("discriminant.png")
+
+train()
+predict()
